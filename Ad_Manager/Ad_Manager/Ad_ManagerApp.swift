6a58004823b1b011f1030f05ff7aebdce3d7a727
AeongKim <jimmy1101@naver.com>
2024-12-26 18:36:29 +0900

    메인화면 (로그인 화면)

--- Ad_Manager/Ad_Manager/Ad_ManagerApp.swift
+++ Ad_Manager/Ad_Manager/Ad_ManagerApp.swift
@@ -5,23 +5,24 @@
 //  Created by 노민철 on 12/26/24.
 //
 
-import SwiftUI
 import FirebaseCore
+import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+    {
+        FirebaseApp.configure()
 
-    return true
-  }
+        return true
+    }
 }
 
 @main
 struct Ad_ManagerApp: App {
     // register app delegate for Firebase setup
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
