b2cf3c9946b6b45c92f576d7dc2cc11c82beae66
nnmmnn15 <shals1645@naver.com>
2024-12-26 14:00:37 +0900

    project

---
+++ Ad_Manager/Ad_Manager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ad_Manager
+//
+//  Created by 노민철 on 12/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(content: {
+            
+        })
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
