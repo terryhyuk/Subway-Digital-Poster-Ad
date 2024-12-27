b43aed06452186d25b1cdec126e2d43960fed362
nnmmnn15 <shals1645@naver.com>
2024-12-27 11:24:49 +0900

    파이썬 - 강남역 추가

--- Python_Server/ml_predict.py
+++ Python_Server/ml_predict.py
@@ -20,13 +20,14 @@ def getDateInfo(selectDate:datetime):
     # 한국 공휴일 설정
     kr_holidays = holidays.KR(years=range(y,y+1))
     holi = selectDate.date() in kr_holidays  # 공휴일 여부
-    return (y,m,d,w,h,holi)
+    weekend = 1 if selectDate.weekday() >= 5 else 0,
+    return (y,m,d,w,h,holi,weekend)
 
 def jamsil(year, month, day, time):
     """
     머신러닝의 잠실의 승하차 예측 수를 리턴하는 함수
     Parameter : 년, 월, 일, 시간대
-    return : list(dict)
+    return : dict
     """
     # Feature ["시간대","월","요일","공휴일여부","일"]] Target ['승객수']
     c_on = joblib.load("model/jamsil/cheong_on.pkl")
@@ -39,7 +40,7 @@ def jamsil(year, month, day, time):
 
     selectDate = datetime(year,month,day,time)
 
-    y,m,d,w,h,holi = getDateInfo(selectDate)
+    y,m,d,w,h,holi,weekend = getDateInfo(selectDate)
 
     # 학습 Feature에 맞게 값 변화
     if h < 3:
@@ -63,7 +64,7 @@ def hongdae(year, month, day, time):
     """
     머신러닝의 홍대의 이용객 수를 예측한 값을 리턴하는 함수
     Parameter : 년, 월, 일, 시간대
-    return : list(dict)
+    return : dict
     """
 
     foreigner_youth = joblib.load("model/hongdae/gradient_boosting.joblib")
@@ -80,4 +81,22 @@ def hongdae(year, month, day, time):
     keys = ["우대권", "20대", "30/40대", "50대", "외국인", "청소년"]
     result_list = [round(norm_woo_result[0][i]) for i in [1, 3, 4, 5]] + [round(foreigner_youth_result[0][i]) for i in [0,2]]
     age_group_dict = {key: round(value) for key, value in zip(keys, result_list)}
+    return age_group_dict
+
+def gangnam(year, month, day, time):
+    """
+    머신러닝의 강남의 이용객 수를 예측한 값을 리턴하는 함수
+    Parameter : 년, 월, 일, 시간대
+    return : dict
+    """
+    selectDate = datetime(year,month,day,time)
+    y,m,d,w,h,holi,weekend = getDateInfo(selectDate)
+
+    gangnam_model = joblib.load("model/gangnam/model.joblib")
+
+    # print(year, month, day, time, w, weekend, holi)
+    gangnam_result = gangnam_model.predict(np.array([year, month, day, time, w, weekend[0], holi]).reshape(-1,7))
+    # print(gangnam_result)
+    keys = ["10대", "20대", "30대", "40대", "50대 이상", "우대권", "외국인"]
+    age_group_dict = {key: round(value) for key, value in zip(keys, gangnam_result[0])}
     return age_group_dict
