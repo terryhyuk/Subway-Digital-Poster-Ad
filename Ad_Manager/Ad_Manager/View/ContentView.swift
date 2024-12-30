2ec5caa06710dad9fede985b5eb7474831eb327f
AeongKim <jimmy1101@naver.com>
2024-12-30 15:24:36 +0900

    메인화면 수정

--- Ad_Manager/Ad_Manager/View/ContentView.swift
+++ Ad_Manager/Ad_Manager/View/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             if loginManager.isLoggedIn {
-                MainView()
+                TabbarView()
             } else {
                 VStack {
                     Spacer()
