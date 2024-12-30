c228f954dd76db3e0bd2e1ae967e1b1576065773
nnmmnn15 <shals1645@naver.com>
2024-12-30 15:04:13 +0900

    Merge Fixed

---
+++ Ad_Manager/Ad_Manager/View/InsertPost.swift
@@ -0,0 +1,18 @@
+//
+//  InsertView.swift
+//  Ad_Manager
+//
+//  Created by 노민철 on 12/30/24.
+//
+
+import SwiftUI
+
+struct InsertPost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InsertPost()
+}
